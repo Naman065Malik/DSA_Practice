b426d6dd0313453a47db3c625eb95ff637ac4f1c
Naman Malik <86320160+Naman065Malik@users.noreply.github.com>
2025-01-04 21:19:47 +0000

    Array.h File Completed

--- DSA_Concepts_By_C/Array.h
+++ DSA_Concepts_By_C/Array.h
@@ -9,7 +9,7 @@ void init(int arr[],int n){
 }
 
 void mock(int arr[], int n){
-    for(int i=0; i<n; i++)
+    for(int i=1; i<n; i++)
         arr[i] = (i+10)*i;
 
     return;
@@ -23,3 +23,52 @@ void traverse(int arr[], int n){
     printf("\n");
     return;
 }
+
+void insertAtFirst(int arr[], int * n, int key){
+    *n += 1;
+    for(int i = (*n)-1; i > 0; i--)
+        arr[i] = arr[i-1];
+
+    arr[0] = key;
+    return;
+}
+
+void insertAtEnd(int arr[], int * n, int key){
+    arr[*n] = key;
+    *n += 1;
+    return;
+}
+
+void insertAtPos(int arr[], int* n, int key, int pos){
+    *n += 1;
+    for(int i = (*n)-1; i > pos; i--)
+        arr[i] = arr[i-1];
+
+    arr[pos-1] = key;
+    return;
+}
+
+void deleteAtFirst(int arr[], int* n){
+    for(int i = 0; i < *n - 1; i++ )
+        arr[i] = arr[i+1];
+
+    *n -= 1;
+
+    return;
+}
+
+void deleteAtEnd(int arr[], int* n){
+    *n -= 1;
+    return;
+}
+
+void deleteAtPos(int arr[], int* n, int pos){
+    for(int i = pos-1; i < *n; i++)
+        arr[i] = arr[i+1];
+
+
+    *n -= 1;
+    return;
+}
+
+
